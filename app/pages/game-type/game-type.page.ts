6b49b30242e0951f68a5441d440e9004af722e47
Justin Michelotti <justin@AdminisatorsMBP.fios-router.home>
2016-09-26 16:25:22 -0400

    still adding player entry

--- app/pages/game-type/game-type.page.ts
+++ app/pages/game-type/game-type.page.ts
@@ -19,7 +19,6 @@ export class GameTypePage {
     this.nav.push(PlayerEntryPage); 
   }
 
-
 }
 
 
--- app/pages/player-entry/player-entry.page.ts
+++ app/pages/player-entry/player-entry.page.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WordEntryPage } from '../pages';
+import * as _ from 'lodash';
 
 @Component({
     templateUrl: 'build/pages/player-entry/player-entry.page.html'
 })
 export class PlayerEntryPage {
+    //players = ['steve', 'justin', 'mom' ];
+    players = [{ name: ''}];
+
+
     constructor(public nav: NavController) {
 
     }
 
-    
+    startMultiPlayerPassAndPlayGame() {
+        this.nav.push(WordEntryPage)
+
+    }
+    addPlayer(){
+        console.log('add player', this.players); 
+        this.players.push({ name: ''});
+    }
+
+    removePlayer(player){
+        _.remove(this.players, player);
+    }
 
 }
